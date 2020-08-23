fd2a3cfebdc2da019cec54984899ed39be81e157
Ammar <ammar.badwan95@gmail.com>
2020-08-23 21:26:17 +0300

    finished initial setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.scss';
+
+const App = props =>{
+  return(
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>hi</h1>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
